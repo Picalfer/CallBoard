1152f7570b00fcd334e448ac939ae9733d0e25ec
Arthur Kozhemyakin <trable228@icloud.com>
2023-10-22 18:06:09 +0300

    menu item listener

--- app/src/main/java/com/landfathich/callboard/MainActivity.kt
+++ app/src/main/java/com/landfathich/callboard/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.landfathich.callboard
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.landfathich.callboard.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +29,45 @@ class MainActivity : AppCompatActivity() {
 
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.my_ads -> {
+                Toast.makeText(this, "Выбрано мои объявления", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.car -> {
+                Toast.makeText(this, "Выбрано машины", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.pc -> {
+                Toast.makeText(this, "Выбрано пк", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.smartphone -> {
+                Toast.makeText(this, "Выбрано смартфоны", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.domestic -> {
+                Toast.makeText(this, "Выбрано бытовая техника", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.sign_in -> {
+                Toast.makeText(this, "Выбрано вход", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.sign_up -> {
+                Toast.makeText(this, "Выбрано регистрация", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.sign_out -> {
+                Toast.makeText(this, "Выбрано выход", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
