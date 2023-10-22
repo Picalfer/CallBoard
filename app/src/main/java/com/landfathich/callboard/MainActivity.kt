61122c83cebd123b78647404422bec40b2d332ee
Arthur Kozhemyakin <trable228@icloud.com>
2023-10-22 17:55:43 +0300

    btn for open menu

--- app/src/main/java/com/landfathich/callboard/MainActivity.kt
+++ app/src/main/java/com/landfathich/callboard/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.landfathich.callboard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import com.landfathich.callboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        init()
+    }
+
+    private fun init() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.mainContent.toolbar,
+            R.string.open,
+            R.string.close
+        )
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
