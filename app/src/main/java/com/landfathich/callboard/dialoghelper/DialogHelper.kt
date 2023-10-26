378e5f79b5c6100bd3b757f01ff433151dd056af
Arthur Kozhemyakin <trable228@icloud.com>
2023-10-26 20:51:12 +0300

    reset password

--- app/src/main/java/com/landfathich/callboard/dialoghelper/DialogHelper.kt
+++ app/src/main/java/com/landfathich/callboard/dialoghelper/DialogHelper.kt
@@ -1,6 +1,8 @@
 package com.landfathich.callboard.dialoghelper
 
 import android.app.AlertDialog
+import android.view.View
+import android.widget.Toast
 import com.landfathich.callboard.MainActivity
 import com.landfathich.callboard.R
 import com.landfathich.callboard.accounthepler.AccountHelper
@@ -14,30 +16,66 @@ class DialogHelper(private val activity: MainActivity) {
         val view = binding.root
         builder.setView(view)
 
+        setDialogState(index, binding)
+
+        val dialog = builder.create()
+        binding.btnSignUpIn.setOnClickListener {
+            setOnClickSignUpIn(index, binding, dialog)
+        }
+        binding.btnForgetPassword.setOnClickListener {
+            setOnClickForgetPassword(binding, dialog)
+        }
+
+        dialog.show()
+    }
+
+    private fun setOnClickForgetPassword(binding: SignDialogBinding, dialog: AlertDialog?) {
+        if (binding.etSignEmail.text.isNotEmpty()) {
+            activity.myAuth.sendPasswordResetEmail(binding.etSignEmail.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            activity,
+                            activity.getString(R.string.email_reset_password_was_sent),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            activity,
+                            activity.getString(R.string.reset_password_error),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            dialog?.dismiss()
+        } else {
+            binding.tvDialogMessage.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setDialogState(index: Int, binding: SignDialogBinding) {
         if (index == DialogConst.SIGN_UP_STATE) {
             binding.tvSignTitle.text = activity.getString(R.string.aс_sign_up)
             binding.btnSignUpIn.text = activity.getString(R.string.sign_up_action)
         } else {
             binding.tvSignTitle.text = activity.getString(R.string.aс_sign_in)
             binding.btnSignUpIn.text = activity.getString(R.string.sign_in_action)
+            binding.btnForgetPassword.visibility = View.VISIBLE
         }
+    }
 
-        val dialog = builder.create()
-        binding.btnSignUpIn.setOnClickListener {
-            dialog.dismiss()
-            if (index == DialogConst.SIGN_UP_STATE) {
-                accountHelper.signUpWithEmail(
-                    binding.etSignEmail.text.toString(),
-                    binding.etSignPassword.text.toString()
-                )
-            } else {
-                accountHelper.signInWithEmail(
-                    binding.etSignEmail.text.toString(),
-                    binding.etSignPassword.text.toString()
-                )
-            }
+    private fun setOnClickSignUpIn(index: Int, binding: SignDialogBinding, dialog: AlertDialog?) {
+        dialog?.dismiss()
+        if (index == DialogConst.SIGN_UP_STATE) {
+            accountHelper.signUpWithEmail(
+                binding.etSignEmail.text.toString(),
+                binding.etSignPassword.text.toString()
+            )
+        } else {
+            accountHelper.signInWithEmail(
+                binding.etSignEmail.text.toString(),
+                binding.etSignPassword.text.toString()
+            )
         }
-
-        dialog.show()
     }
 }
